28d32076479ae97220066278ee726772623d9908
sigmotoa <si.galvismotoa@gmail.com>
2026-04-16 15:30:15 -0500

    [UPDATE] using sqlmodel and tables

--- models.py
+++ models.py
@@ -1,19 +1,21 @@
-from pydantic import BaseModel, Field
+# from pydantic import BaseModel, Field
+from email.policy import default
+
 from pokemon_types import PokemonType
 from typing import Optional
+from sqlmodel import SQLModel, Field
 
 
-class PokemonBase(BaseModel):
-    name: str = (Field
-                 (...,
-                  min_length=3,
-                  max_length=64))
-    type: PokemonType = Field(...
-                              )
-    level: int = Field(...,
-                       gt=0,
-                       le=100)
+class PokemonBase(SQLModel):
+    name: str | None = Field(default=None,
+                             min_length=3,
+                             max_length=64)
+    type: PokemonType | None = Field(default=None,
+                                     )
+    level: int | None = Field(default=None,
+                              gt=0,
+                              le=100)
 
 
-class PokemonID(PokemonBase):
-    id: int = Field(..., gt=0)
+class PokemonID(PokemonBase, table=True):
+    id: int | None = Field(default=None, primary_key=True, gt=0)
