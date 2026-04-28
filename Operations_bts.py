be62790a5d305d8f9cb7da64abe091f5249196ec
Dayanna <dlvaron77@ucatolica.edu.co>
2026-04-27 22:33:00 -0500

    [UPDATE] Se modificaron las operaciones para incluir integrantes, albumes y tours

--- Operations_bts.py
+++ Operations_bts.py
@@ -1,59 +1,76 @@
 from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select
+from models import (
+    Integrante, IntegranteBase,
+    Album, AlbumBase, AlbumUpdate,
+    Tour, TourBase
+)
 
-from models import PokemonBase, PokemonID, PokemonUpdate
-
-
-def createPokemon_db(pokemon: PokemonBase, session: Session):
-    new_pokemon = PokemonID.model_validate(pokemon)
-    session.add(new_pokemon)
+#OPERACIONES INTEGRANTES
+def crear_integrante(integrante: IntegranteBase, session: Session):
+    nuevo = Integrante.model_validate(integrante)
+    session.add(nuevo)
     session.commit()
-    session.refresh(new_pokemon)
+    session.refresh(nuevo)
+    return nuevo
 
-    return new_pokemon
+def ver_integrantes(session: Session):
+    return session.exec(select(Integrante)).all()
 
+def buscar_integrante_nombre(nombre: str, session: Session):
+    # Búsqueda parcial e insensible a mayúsculas
+    statement = select(Integrante).where(Integrante.nombre.ilike(f"%{nombre}%"))
+    return session.exec(statement).all()
 
-def show_all_pokemon_db(session: Session):
-    # return session.query(PokemonID).all()
-    # session.exec(select(PokemonID).gr)
-    return session.exec(select(PokemonID))
-    # statement = select(PokemonID)
-    # results = session.exec(statement)
-    # return results
+#OPERACIONES ÁLBUM
+def crear_album(album: AlbumBase, session: Session):
+    nuevo = Album.model_validate(album)
+    session.add(nuevo)
+    session.commit()
+    session.refresh(nuevo)
+    return nuevo
 
+def ver_albumes(session: Session):
+    return session.exec(select(Album)).all()
 
-def find_one_pokemon_db(id: int, session: Session):
+def buscar_album_id(id: int, session: Session):
     try:
-        return session.get_one(PokemonID, id)
+        return session.get_one(Album, id)
     except NoResultFound:
         return None
 
-
-def update_one_pokemon_db(id: int, new_pokemon: PokemonUpdate, session: Session):
-    pokemon=find_one_pokemon_db(id, session)
-    if pokemon is None:
+def editar_album(id: int, datos: AlbumUpdate, session: Session):
+    album = buscar_album_id(id, session)
+    if album is None:
         return None
-    pokemon_update = new_pokemon.model_dump(exclude_unset=True)
-    pokemon.sqlmodel_update(pokemon_update)
-    session.add(pokemon)
+    campos_nuevos = datos.model_dump(exclude_unset=True)
+    album.sqlmodel_update(campos_nuevos)
+    session.add(album)
     session.commit()
-    session.refresh(pokemon)
+    session.refresh(album)
+    return album
 
-    return pokemon
+#  OPERACIONES TOURS
+def crear_tour(tour: TourBase, session: Session):
+    nuevo = Tour.model_validate(tour)
+    session.add(nuevo)
+    session.commit()
+    session.refresh(nuevo)
+    return nuevo
 
+def ver_tours(session: Session):
+    return session.exec(select(Tour)).all()
 
-def kill_one_pokemon_db(id: int, session: Session):
+def buscar_tours_en_ciudad(ciudad: str, session: Session):
+    # Busca tours que contengan la ciudad (búsqueda parcial)
+    statement = select(Tour).where(Tour.ciudades_visitadas.ilike(f"%{ciudad}%"))
+    return session.exec(statement).all()
+
+def cancelar_tour(id: int, session: Session):
     try:
-        pokemon=session.get_one(PokemonID, id)
-        session.delete(pokemon)
+        tour = session.get_one(Tour, id)
+        session.delete(tour)
         session.commit()
-        return pokemon
+        return tour
     except NoResultFound:
         return None
-
-
-
-
-
-
-
